6b2b1764ec380e3d5f3426e6a0560ffaf3ca8d82
odedh <odedh@neureality.ai>
2024-12-01 23:05:58 +0200

    Changes

--- main.py
+++ main.py
@@ -2,21 +2,31 @@ from inverted_index import InvertedIndex
 from boolean_retrieval import BooleanRetrieval
 import pyfiglet
 
+
 def main():
+    retrievaled_q_str = ""
+    # Part 1
     print(pyfiglet.figlet_format("PART 1"))
     # parser = InvertedIndex('./dataset')
-    parser = InvertedIndex('textretrieval-exc1\dataset')
-    parser.extract_docs(limit=40)
-    parser.extract_indices_from_text(limit=500)
+    parser = InvertedIndex('dataset')
+    parser.extract_docs()
+    parser.extract_indices_from_text()
     # parser.print_top_n_words(n=20)
     # parser.print_bottom_n_words(n=20)
 
+    # Part 2
     print(pyfiglet.figlet_format("PART 2"))
-    q_path = 'textretrieval-exc1\BooleanQueries.txt'
+    q_path = 'BooleanQueries.txt'
     b_retiver = BooleanRetrieval(parser, queries_path=q_path)
     for query in b_retiver.queries():
         print(f"Query: {query}")
-        print(f"Result: {b_retiver.process_query_rpn(query)}")
+        # print(f"Result: {b_retiver.process_query_rpn(query)}")
+        retrievaled_q_str += " ".join([item[1] for item in b_retiver.process_query_rpn(query)]) + "\n"
     
-if __name__=="__main__":
+    with open('Part_2.txt', 'w') as f:
+        f.write(retrievaled_q_str)
+
+
+if __name__ == "__main__":
     main()
+    
