ee4f17ad0c77ce7bcc099c1660cd3ec84add0ca7
odedh <odedh@neureality.ai>
2024-12-01 18:12:16 +0200

    Refactor main function to integrate Boolean retrieval and update document extraction limits

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from inverted_index import InvertedIndex
+from boolean_retrieval import BooleanRetrieval
+import pyfiglet
 
 def main():
-    parser = InvertedIndex('./dataset')
-    parser.extract_docs(limit=30)
-    parser.extract_indices_from_text(limit=3000)
-    parser.print_top_n_words(n=20)
-    parser.print_bottom_n_words(n=20)
+    print(pyfiglet.figlet_format("PART 1"))
+    # parser = InvertedIndex('./dataset')
+    parser = InvertedIndex('textretrieval-exc1\dataset')
+    parser.extract_docs(limit=40)
+    parser.extract_indices_from_text(limit=500)
+    # parser.print_top_n_words(n=20)
+    # parser.print_bottom_n_words(n=20)
 
+    print(pyfiglet.figlet_format("PART 2"))
+    q_path = 'textretrieval-exc1\BooleanQueries.txt'
+    b_retiver = BooleanRetrieval(parser, queries_path=q_path)
+    for query in b_retiver.queries():
+        print(f"Query: {query}")
+        print(f"Result: {b_retiver.process_query_rpn(query)}")
+    
 if __name__=="__main__":
     main()
